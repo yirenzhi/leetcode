22de2133be932bd709cec47fe838f4dcdba4ec26
yirenzhi <yiren_zhi@163.com>
2016-07-26 15:16:14 +0800

    new repository for leetcode

---
+++ leetcode_python/100SameTree.py
@@ -0,0 +1,17 @@
+Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+
+if __name__=='__main__':
+    print("abb')
