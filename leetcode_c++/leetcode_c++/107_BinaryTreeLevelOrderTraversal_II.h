a4a9b0ed0ba9829eada47ca91076b63e0e5bd366
yirenzhi <yiren_zhi@163.com>
2020-09-07 00:34:36 +0800

    新增107

---
+++ leetcode_c++/leetcode_c++/107_BinaryTreeLevelOrderTraversal_II.h
@@ -0,0 +1,74 @@
+#pragma once
+#pragma once
+#include "base.h"
+
+
+//107. 二叉树的层序遍历  后倒叙
+//102题最后的数组反转即可得到这题的答案
+class Solution107 :public Solution {
+	vector<vector<int>> levelOrderBottom(TreeNode* root) {
+		vector<vector<int>> vecNum;
+		if (!root)
+		{
+			return vecNum;
+		}
+		queue<TreeNode*> q;
+		q.push(root);
+		while (q.size()>0)
+		{
+			int len = q.size();
+			vector<int> order;
+			for (int i = 0; i<len; i++)
+			{
+				TreeNode* temp = q.front();
+				q.pop();
+				order.push_back(temp->val);
+				if (temp->left) {
+					q.push(temp->left);
+				}
+				if (temp->right) {
+					q.push(temp->right);
+				}
+			}
+			vecNum.push_back(order);
+		}
+		reverse(vecNum.begin(), vecNum.end());
+		return vecNum;
+	}
+public:
+
+	void test()
+	{
+		/*
+		示例：
+		二叉树：[3, 9, 20, null, null, 15, 7],
+
+		3
+		/ \
+		9  20
+		/ \
+		15   7
+
+		*/
+		TreeNode* root = new TreeNode(3);
+		root->left = new TreeNode(9);
+		root->right = new TreeNode(20);
+		root->left->left = new TreeNode(15);
+		root->left->right = new TreeNode(7);
+
+		vector<vector<int>> vecNum;
+		vecNum = levelOrder(root);
+
+		
+		for (size_t i = 0; i < vecNum.size(); i++)
+		{
+			for (size_t j = 0; j < vecNum[i].size(); j++)
+			{
+				cout << vecNum[i][j] << " ";
+			}
+			cout << endl;
+		}
+
+
+	}
+};
--- leetcode_c++/leetcode_c++/start.cpp
+++ leetcode_c++/leetcode_c++/start.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include"LeetCode.h"
 #include"199.BinaryTreeRightSideView.h"
 #include"21.MergeTwoSortedLists.h"
 #include"102.BinaryTreeLevelOrderTraversal.h"
+#include"107_BinaryTreeLevelOrderTraversal_II.h"
 using namespace std;
 #define add(a,b) a+b
 #define A(x) return Solution##x
@@ -9,21 +11,70 @@ Solution* createSolutin(int num)
 {
 	switch (num)
 	{
-		case 199:
-		{
-			return new Solution199;
-		}
+	case 3:
+	{
+		//LeetCode::leetcode3();
+	}
+	break;
+	case 5:
+	{
+
+	}
+	break;
+	case 6:
+	{
+
+	}
+	break;
+	case 8:
+	{
+
+	}
+	break;
+	case 12:
+	{
+
+	}
+	break;
+	case 14:
+	{
+
+	}
+	break;
+	case 15:
+	{
+
+	}
+	break;
+	case 16:
+	{
+
+	}
+	break;
+
+	case 199:
+	{
+		return new Solution199;
+	}
+	break;
+	case 21:
+	{
+		return new Solution21;
+	}
+	break;
+	case 102:
+	{
+		return new Solution102;
+	}
+	break;
+	case 107:
+	{
+		return new Solution107;
+	}
+	break;
+
+	default:
 		break;
-		case 21:
-		{
-			return new Solution21;
-		}
-		case 102:
-		{
-			return new Solution102;
-		}
-		default:
-			break;
 	}
 	return NULL;
 }
